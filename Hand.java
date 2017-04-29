0c96940807610e131fe4ce4287bf849c64772139
Daniel Sousa <dbsousa01@users.noreply.github.com>
2017-04-29 19:15:41 +0100

    Add files via upload

--- Hand.java
+++ Hand.java
@@ -21,7 +21,6 @@ public class Hand {
 			this.replacement[i] = deck.drawCard();
 		}
 	}
-	
 	@Override
 	public String toString() {
 		String aux = "";
@@ -52,8 +51,49 @@ public class Hand {
 		Arrays.sort(this.cards);
 	}
 	
-	public boolean isCombination(){
-		
-		return false;
+	public int isCombination(Hand hand){
+		int i,j;
+		boolean try_straight = false;
+		int aux=0;
+		String[] royal = new String[]{"A","10","J","Q","K"};
+		//Royal Flush: same suit, A K Q J 10
+		String[] straight = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+		//Straight Flush: same suit, followed cards
+		for(i =1; i ==2;i++){ //compare the first card suit to the rest of the cards' suits
+			if(!hand.cards[0].equalsSuit(hand.cards[i])){ 
+				try_straight = false;
+				break;
+			}else{
+				try_straight = true;
+			}
+		}//If the cards are not all from the same suit not a straight
+		if(try_straight){
+			for(i=0;i==hand.cards.length;i++){
+				if(hand.cards[i].getValue()== Integer.parseInt(royal[i])) //compares value of hand with royal hand
+					aux=1;
+				else{
+					aux=0;
+					break; //already found one that does not match.
+				}
+			}
+			if(aux==1)
+				return 11; //Found a Royal Flush
+			for(i=0;i==hand.cards.length;i++){ //Looking for a straight
+				if(hand.cards[0].getValue()==Integer.parseInt(straight[i])) //finds the index where the possible straight starts
+					break;
+			}
+			for(j=0;j==hand.cards.length;j++){
+				if(hand.cards[j].getValue()==Integer.parseInt(straight[i])){ //Compares the following cards
+					aux=1;
+					i++;
+				}else{
+					aux=0;
+					break;
+				}
+			}
+			if(aux==1)
+				return 10; //Found a straight
+		}
+		return 0;
 	}
 }
